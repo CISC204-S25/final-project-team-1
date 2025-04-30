99a593b2bf84af08abe30b8c9ecbed00e2904c0d
Davidghbldlsh <davidgh@udel.edu>
2025-04-30 16:35:43 -0400

    Character
    Might not work, need to go further in detail

---
+++ game-final-project-1/character_body_2d.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@export var push_force = 50.0
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	push_objects()
+
+func push_objects():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider is RigidBody2D:
+			collider.apply_central_impulse(collision.get_normal() * -1 * push_force)
+				
