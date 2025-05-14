d61cb790968c1631dd9ce915389895530d340408
Hanamato <hanamato@udel.edu>
2025-05-14 13:13:22 -0400

    Bg music, frog noise

--- team1_finalproject/player_1_jumper.gd
+++ team1_finalproject/player_1_jumper.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
 @onready var sprite = $AnimatedSprite2D
+@onready var frog_sound = $Ribbit
 
 
 
@@ -17,6 +18,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		frog_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
